f9035d66674e08be69d26512d9140adfa3ad5ee6
Fernando Soto <fersoto117@gmail.com>
2018-01-28 17:31:01 -0600

    Really add main fragment

--- random-numbers/app/src/main/java/com/zomaotoko/randomnumbers/MainActivity.kt
+++ random-numbers/app/src/main/java/com/zomaotoko/randomnumbers/MainActivity.kt
@@ -3,7 +3,11 @@ package com.zomaotoko.randomnumbers
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+
+import com.zomaotoko.randomnumbers.Utils.Companion.dpToPx
 
 
 class MainActivity : Activity() {
@@ -11,5 +15,22 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Menu layout must be 48 dp smaller than screen width
+        setMenuLayoutWidth(getScreenWidthInDp() - dpToPx(48F))
+    }
+
+    fun onTitleBarButtonClick(view: View) {
+        drawerLayout.openDrawer(menuLayout)
+    }
+
+    private fun setMenuLayoutWidth(width: Int) {
+        menuLayout.layoutParams.width = width
+    }
+
+    private fun getScreenWidthInDp() : Int {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.widthPixels
     }
 }
---
+++ random-numbers/app/src/main/java/com/zomaotoko/randomnumbers/Utils.kt
@@ -0,0 +1,20 @@
+package com.zomaotoko.randomnumbers
+
+import android.content.res.Resources
+import android.util.DisplayMetrics
+
+
+class Utils private constructor() {
+
+    companion object {
+        fun pxToDp(pixels: Int) : Float {
+            val displayMetrics: DisplayMetrics = Resources.getSystem().displayMetrics
+            return pixels.toFloat() * DisplayMetrics.DENSITY_DEFAULT / displayMetrics.densityDpi
+        }
+
+        fun dpToPx(dp: Float) : Int {
+            val displayMetrics: DisplayMetrics = Resources.getSystem().displayMetrics
+            return (dp * displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT).toInt()
+        }
+    }
+}
