3e939271fa028a5e4b7a5258916b23f17959524a
Fernando Soto <fersoto117@gmail.com>
2018-01-31 21:09:32 -0600

    Add listener to button

--- random-numbers/app/src/main/java/com/zomaotoko/randomnumbers/GenerateNumberFragment.kt
+++ random-numbers/app/src/main/java/com/zomaotoko/randomnumbers/GenerateNumberFragment.kt
@@ -7,10 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import kotlinx.android.synthetic.main.fragment_generate_number.*
+
 
 class GenerateNumberFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_generate_number, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        generateNumberBtn.setOnClickListener { showText("Number") }
+    }
+
+    private fun showText(text: String) {
+        if (isAdded) activity.runOnUiThread({ numberTxt.text = text })
     }
 }
